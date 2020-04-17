00f0fbaf404d4d6602a3841edda76b389d69c956
amine-lamti <60108149+amine-lamti@users.noreply.github.com>
2020-04-17 23:09:35 +0100

    vvvvvv

--- client/src/components/Clientcar.js
+++ client/src/components/Clientcar.js
@@ -1,36 +1,28 @@
-import React, { Component } from 'react';
-import { connect } from "react-redux"
-import { getclientcar } from "../actions/CarActions"
-import { Link } from "react-router-dom"
-import {Card,Button} from "react-bootstrap"
-
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getclientcar } from "../actions/CarActions";
+import CarItem from "./CarItem";
 
 class Clientcar extends Component {
-
-
-    componentDidMount() {
-        this.props.getclientcar()
-    }
-    render() {
-        return (
-            <div className="cars">{this.props.car.map(el =><div className="car"><Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={el.image} />
-            <Card.Body>
-        <Card.Title>{el.modele}</Card.Title>
-              <Card.Text>
-               {el.prix}
-              </Card.Text>
-              <Button variant="dark" className="butn"><Link className="butn" to={`/caritem/${el._id}`}>See more</Link></Button>
-            </Card.Body>
-          </Card></div>) }</div>  
-        );
-    }
+  componentDidMount() {
+    this.props.getclientcar();
+  }
+  render() {
+    return (
+      <div>
+        {this.props.car.map((el) => (
+          <div>
+            <CarItem id={el._id} />
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
-const mapStateToProps = state => {
-    return {
-        car: state.Clientcar.cars
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    car: state.Clientcar.cars,
+  };
+};
 
 export default connect(mapStateToProps, { getclientcar })(Clientcar);
